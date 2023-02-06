f90bb372115b1d1c43d1386b95f8716f13b19fce
Vaishali <vaishalimehmi@yahoo.co.uk>
2023-02-06 19:15:29 +0000

    challenge update

---
+++ cinema_management.py
@@ -0,0 +1,50 @@
+username = input("Enter username:")
+print("Username is: " + username)
+
+
+"""
+Cinema age management system
+if someone is under 12 
+        U, PG and 12 films available
+
+if someone is under 15
+        U, PG, 12 and 15 films available
+
+if over 18
+        All available
+"""
+
+
+def which_films_are_suitable(user_age):
+    if user_age <= 12:
+        print("Thank you, you can watch all U, PG and 12 rated films")
+        
+    elif user_age <= 15:
+        print("Thank you, you can watch all U, PG, 12 and 15 rated films")
+    else:
+        print("""Thank you, you can watch all films!!
+              U, PG, 12, 15 and 18 rated films""")
+        
+which_films_are_suitable()
+ 
+ 
+"""
+ Chatbot greetings
+ 
+ btwn 05:00 - 12:00  Good Morning
+ btwn 12:00 - 18:00   Good Afternoon
+ btwn 18:00 - 12:00 Good Evening
+ """  
+
+# 24hr clock 
+time = 26
+ 
+
+if time > 5 and time < 12:
+    print("Good Morning")
+elif time > 12 and time < 18:
+    print("Good Afternoon")
+elif  time > 18 and time < 24:
+    print("Good Evening")
+else:
+    print("Please check your timezone!")
---
+++ sum_integers.py
@@ -0,0 +1,21 @@
+
+# def solution(n):
+#     list_1 = [int(x) for x in str(n)]   
+#     print(sum(list_1))
+    
+# solution(29)
+
+
+def solution(n):
+    list_1 = [int(x) for x in str(n)]
+    total_sum = sum(list_1)
+    return total_sum
+
+def verify(result):
+    print(result)
+    
+
+# output = solution(2344)
+
+# verify(output)
+
